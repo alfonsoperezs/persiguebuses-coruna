ddbe4b6ee091ed3f45e2004cf7c31e9e371cc1ee
alfonsoperezs <fonpesan@gmail.com>
2025-06-05 22:09:09 +0200

    fix(api): add stops to detect cocheras buses

--- backend/app/services.py
+++ backend/app/services.py
@@ -16,12 +16,12 @@ def get_lines():
 def get_buses():
     lines = get_lines()
     buses = {}
-    bus_stop_list = [25, 87, 181, 115, 286]
+    bus_stop_list = [25, 87, 181, 115, 286, 424, 434]
 
     for bus_stop in bus_stop_list:
         data = get_query_itranvias(func=0, dato=bus_stop).json()
-        for line in data["buses"]["lineas"]:
-            line_name = lines[line["linea"]]
+        for line in data["buses"].get("lineas", {}):
+            line_name = lines.get(line["linea"], "Uknown")
             for bus in line.get("buses", []):
                 if bus["bus"] not in buses:
                     buses[bus["bus"]] = {"line" : line_name}
