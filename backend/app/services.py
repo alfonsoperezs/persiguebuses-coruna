cd0e16e966ff430a9818c141281bd06ce25fbde0
Alfonso <alfonso.perezsanchez@hotmail.com>
2025-07-05 12:55:09 +0200

    feat(backend): add exception messages

--- backend/app/services.py
+++ backend/app/services.py
@@ -7,15 +7,18 @@ def get_lines() -> dict:
 
     :returns: A dict with the line's id and name.
     """
-    response = get_query_itranvias(func=1, dato=None)
-    if response.ok:
-        data = response.json()
-        lines = {}
-        for line in data["lineas"]:
-            line_id = int(line["id"])
-            lines[line_id] = line["nom_comer"]
-        return lines
-    else:
+    try:
+        response = get_query_itranvias(func=1, dato=None)
+        if response.ok:
+            data = response.json()
+            lines = {}
+            for line in data["lineas"]:
+                line_id = int(line["id"])
+                lines[line_id] = line["nom_comer"]
+            return lines
+        else:
+            return {}
+    except:
         return {}
 
 def get_buses() -> dict:
@@ -26,16 +29,19 @@ def get_buses() -> dict:
     """
     lines = get_lines()
     buses = {}
-    bus_stop_list = [25, 87, 181, 115, 286, 424, 434]
+    bus_stop_list = [25, 87, 181, 115, 286, 424, 434] # Minimun stops to catch buses from all lines.
 
-    for bus_stop in bus_stop_list:
-        data = get_query_itranvias(func=0, dato=bus_stop).json()
-        for line in data["buses"].get("lineas", {}):
-            line_name = lines.get(line["linea"], "Uknown")
-            for bus in line.get("buses", []):
-                if bus["bus"] not in buses:
-                    buses[bus["bus"]] = {"line" : line_name}
-    return dict(sorted(buses.items()))
+    try:
+        for bus_stop in bus_stop_list:
+            data = get_query_itranvias(func=0, dato=bus_stop).json()
+            for line in data["buses"].get("lineas", {}):
+                line_name = lines.get(line["linea"], "Uknown")
+                for bus in line.get("buses", []):
+                    if bus["bus"] not in buses:
+                        buses[bus["bus"]] = {"line" : line_name}
+        return dict(sorted(buses.items()))
+    except:
+        return {"error": "No available buses at the moment"}
 
 def get_bus_details(id: int) -> dict:
     """
@@ -44,8 +50,11 @@ def get_bus_details(id: int) -> dict:
     :param id: The bus id.
     :returns: A dict with the bus details.
     """
-    bus_details = get_bus_by_id(id)
-    return bus_details.to_dict()
+    try:
+        bus_details = get_bus_by_id(id)
+        return bus_details.to_dict()
+    except:
+        return {"error": f"No available details for {id} bus."}
 
 def get_bus_position(bus_id: int, line: str) -> dict:
     """
@@ -55,22 +64,28 @@ def get_bus_position(bus_id: int, line: str) -> dict:
     :param line: The line name. Ex: 1A, 12...
     :returns: A dict with the coordenates.
     """
-    lines = get_lines()
+    try:
+        lines = get_lines()
 
-    for key, value in lines.items():
-        if value == line:
-            line_id = key
+        for key, value in lines.items():
+            if value == line:
+                line_id = key
 
-    response = get_query_itranvias(func=99, dato=line_id)
+        response = get_query_itranvias(func=99, dato=line_id)
 
-    if response.ok:
-        data = response.json()
-        for map in data['mapas']:
-            for buses in map['buses']:
-                for bus in buses['buses']:
-                    id = bus['bus']
-                    if int(id) == int(bus_id):
-                        posx = bus['posx']
-                        posy = bus['posy']
-                        return {"bus": id, "position": {"pos_x": posx, "pos_y": posy}}
+        if response.ok:
+            data = response.json()
+            for map in data['mapas']:
+                for buses in map['buses']:
+                    for bus in buses['buses']:
+                        id = bus['bus']
+                        if int(id) == int(bus_id):
+                            posx = bus['posx']
+                            posy = bus['posy']
+                            return {"bus": id, "position": {"pos_x": posx, "pos_y": posy}}
+        else:
+            return {"error": f"No available position for {bus_id} bus."}
+
+    except:
+        return {"error": f"No available position for {bus_id} bus."}
                    
