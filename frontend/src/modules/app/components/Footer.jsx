ff2f3a58339fd1ccd97be12294c473a6597e46dc
Alfonso <alfonso.perezsanchez@hotmail.com>
2025-07-08 17:22:31 +0200

    fix(frontend): correct footer display

--- frontend/src/modules/app/components/Footer.jsx
+++ frontend/src/modules/app/components/Footer.jsx
@@ -3,9 +3,9 @@ import Lang from './Lang'
 const Footer = () => {
     return(
         <footer>
-            <div className="p-2 bg-danger d-flex justify-content-center gap-3">
-                <p>© 2025 Persiguebuses</p>
-                <p> - </p>
+            <div className="bg-danger d-flex justify-content-center align-items-center gap-3 h-100">
+                <p className='m-0'>© 2025 Persiguebuses</p>
+                <p className='m-0'> - </p>
                 <Lang/>
             </div>
         </footer>
